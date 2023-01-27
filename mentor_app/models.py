2e87cde662ecf1176bf4d0834c1ae28756c19424
ilsaf <justsairex@gmail.com>
2023-01-27 15:49:10 +0300

    Add models Area, Modules

--- mentor_app/models.py
+++ mentor_app/models.py
@@ -9,3 +9,20 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+
+
+class Area(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Module(models.Model):
+    name = models.CharField(max_length=100)
+    grade = EnumField(enums.GradeEnum, null=True, default=None)
+
+    area = models.ForeignKey(Area, on_delete=models.CASCADE, null=True)
+
+    def __str__(self):
+        return self.name
