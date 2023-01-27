33e8369b1b8747464795a42e6ddbad002082a8af
ilsaf <justsairex@gmail.com>
2023-01-27 15:50:16 +0300

    Add permission for UserViewSet

--- mentor_app/views.py
+++ mentor_app/views.py
@@ -1,8 +1,10 @@
-from rest_framework import viewsets, permissions
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from . import models
 from . import serializers
 
 
-class UserViewSet(viewsets.ModelViewSet):
+class UserViewSet(ModelViewSet):
     queryset = models.User.objects.all()
     serializer_class = serializers.UserSerializers
+    permission_classes = (IsAuthenticated,)
